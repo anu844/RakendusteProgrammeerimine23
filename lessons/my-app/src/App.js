426edc896a41963df6041c6c2d792623ff6d5e3c
Anu Sarapuu <114922100+anu844@users.noreply.github.com>
2023-09-17 20:14:58 +0300

    Lisatud my-app-i nimi ja hobid

--- lessons/my-app/src/App.js
+++ lessons/my-app/src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const initialHobbies = ['Muusika kuulamine', 'Heegeldamine', 'Taimede kasvatamine', 'Looduses jalutamine', 'Lugemine', 'Joonistamine', 'Puslede kokkupanemine', 'Koeraga mängimine', 'Ristsõnade lahendamine', 'Netflixi vaatamine', 'Kingituste tegemine', 'Küpsetamine'];
+
+  // Kasuta useState'i, et luua dünaamiline nimekiri hobidest
+  const [hobbies, setHobbies] = useState(initialHobbies);
+
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Tervitused
+        <h1>Anu Sarapuu</h1>
+        <a className="allajoonitud">
+          Minu hobid:
         </a>
+        <ul>
+          {hobbies.map((hobi, index) => (
+            <li key={index}>{hobi}</li>
+          ))}
+        </ul>
+      
+        <p>
+          
+        </p>
+        
       </header>
     </div>
   );
