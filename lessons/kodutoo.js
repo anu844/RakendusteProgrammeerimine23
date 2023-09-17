c90bd3598ac1aeef438cae40b8836e476dd71d3f
Anu Sarapuu <114922100+anu844@users.noreply.github.com>
2023-09-17 18:30:12 +0300

    Lisatud async/await kirjeldused ja näidised

--- lessons/kodutoo.js
+++ lessons/kodutoo.js
@@ -1,3 +1,4 @@
+// == <- EQUALITY
 // == on true kui mõlema poole objektid on samad
 // string - true kui mõlemal poolel on samad trükimärgid samas järjekorras
 // number - true kui mõlema poole väärtused on samad
@@ -22,10 +23,85 @@
 // console.log(object1 === object2);
 // console.log(object1 === object1);
 
-//===
-// 
-console.log(1 === 1);
-console.log('hello' === 'hello');
-console.log('1' === 1);
-console.log(0 === false);
-console.log(null === null);
+//=== <- STRICT EQUALITY
+// true, kui pooled on täpselt samad
+// false, kui pooled on erinevat tüüpi
+// number - true - molemad pooled peavad olema samad
+// string - true - molemal pool peavad olema samad trükimärgid ja samas järjekorras
+// boolean - true - mõlemad pooled kad "true" või " false"
+// console.log(1 === 1);
+// console.log('hello' === 'hello');
+// console.log('1' === 1);
+// console.log(0 === false);
+// console.log(null === undefined);
+// console.log("------------------------------")
+
+// ASYNC ja AWAIT
+
+// function resolveAfter2Seconds(){
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve(1+1);
+//         }, 2000);
+//     })
+// }
+
+// async function asyncCall() {
+//     console.log('calling');
+//     const result = await resolveAfter2Seconds();
+//     console.log(result);
+
+// }
+
+// asyncCall();
+
+// Async - kasutatakse sündmustepõhiste operatsioonide või aeganõudvate ülesannete juhtimiseks veebirakendustes.
+// Kasutatakse olukordades, kus tegevused võivad võtta aega ja ei soovi, et need tegevused blokeeriksid veebilehe kasutajaliidest.
+
+// Callbacks - vanim viis asünkroonseid ülesandeid käsitada.
+// Funktsioonid edastatakse teisele funktsioonile ja kutsutakse hiljem tagasi, kui esimene ülesanne on lõpetatud.
+
+function tegevusAsünkroonne1(callback) {
+    setTimeout(function() {
+      console.log("Asünkroonse tegevuse lõpp1");
+      callback();
+    }, 10000);
+  }
+  
+  function pärastTegevust() {
+    console.log("Tegevus lõppes1!");
+  }
+  
+  tegevusAsünkroonne1(pärastTegevust);
+  
+// Promise - objekt, mis esindab tulevast väärtust või veateadet. 
+// Pakub selget viisi asünkroonse tegevuse tulemuste haldamiseks.
+
+  function tegevusAsünkroonne2() {
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        console.log("Asünkroonse tegevuse lõpp2");
+        resolve();
+      }, 3000);
+    });
+  }
+  
+  tegevusAsünkroonne2().then(function() {
+    console.log("Tegevus lõppes2!");
+  });
+
+  // Async/Await - kaasaegsem viis asünkroonsete ülesannete haldamiseks JavaScriptis.
+  // Saab määrata funktsiooni märksõnaga 'async' ja kasutada 'await' märksõna, et oodata asünkroonse tegevuse lõppemist.
+
+  async function tegevusAsünkroonne3() {
+    await new Promise(function(resolve) {
+      setTimeout(function() {
+        console.log("Asünkroonse tegevuse lõpp3");
+        resolve();
+      }, 11000);
+    });
+    console.log("Tegevus lõppes3!");
+  }
+  
+  tegevusAsünkroonne3();
+  
