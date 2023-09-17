d6cfe939e281f94b8cd687fccd73a343a71ea950
Anu Sarapuu <114922100+anu844@users.noreply.github.com>
2023-09-17 15:56:19 +0300

    Lisatud == ja === näited

---
+++ lessons/kodutoo.js
@@ -0,0 +1,31 @@
+// == on true kui mõlema poole objektid on samad
+// string - true kui mõlemal poolel on samad trükimärgid samas järjekorras
+// number - true kui mõlema poole väärtused on samad
+// boolean - true kui mõlemad pooled on "true" või "false"
+
+// console.log(1==1);
+// console.log('hello' == 'hello');
+// console.log('1' == 1);
+// console.log(0 == false);
+// console.log(1.05==1.05);
+// console.log(null == undefined);
+
+// const object1 = {
+//     key: "value",
+// };
+// const object2 = {
+//     key: "value",
+// };
+// console.log("------------------------------")
+// console.log(object1 == object2);
+// console.log(object1 == object1);
+// console.log(object1 === object2);
+// console.log(object1 === object1);
+
+//===
+// 
+console.log(1 === 1);
+console.log('hello' === 'hello');
+console.log('1' === 1);
+console.log(0 === false);
+console.log(null === null);
