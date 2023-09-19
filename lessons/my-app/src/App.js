2d754189866db92804a68de08e44810f5c0ca230
Anu Sarapuu <114922100+anu844@users.noreply.github.com>
2023-09-19 21:31:38 +0300

    Lisatud input lahtrid

--- lessons/my-app/src/App.js
+++ lessons/my-app/src/App.js
@@ -1,14 +1,44 @@
 
 import './App.css';
-
+import React, { useState } from 'react';
 
 function App() {
+
+const [firstName, setfirstName] = useState('');
+const [lastName, setlastName] = useState('');
+const [schoolName, setschoolName]= useState('');
+const [displayData, setDisplayData] = useState('');
+
+
+const saveLocal = () => {
+  localStorage.setItem('eesnimi', firstName);
+  localStorage.setItem('perenimi', lastName);
+  localStorage.setItem('koolinimi', schoolName);
+  console.log('Andmed salvestati');
+
+  sessionStorage.setItem('eesnimi', firstName);
+  sessionStorage.setItem('perenimi', lastName);
+  sessionStorage.setItem('koolinimi', schoolName);
+  
+  let data = {
+      firstName: firstName,
+      lastName: lastName,
+      schoolName: schoolName
+  }
+
+  localStorage.setItem('personData', JSON.stringify(data));
+
+  document.cookie = "schoolName="+schoolName;
+
+  setDisplayData(`Eesnimi: ${firstName}, Perenimi: ${lastName}, Kooli nimi: ${schoolName}`);
+};
   
   return (
     
     <div className="App">
       <header className="App-header">
         <h1>Anu Sarapuu</h1>
+
         <a className="allajoonitud">
           MINU HOBID
         </a>
@@ -19,7 +49,18 @@ function App() {
           <td style={{ textAlign: 'left' }}>► Puslede kokkupanemine <br />► Koeraga mängimine <br /> ► Ristsõnade lahendamine <br />► Netflixi vaatamine <br />► Kingituste tegemine <br />► Küpsetamine</td>
           </tr>
         </table>
-        
+        <br />
+
+        <label htmlFor="firstName" style={{ fontSize: '20px'}}>Sisesta oma eesnimi:</label>
+        <input type="text" name="firstName" id="firstName" onChange={(e) => setfirstName(e.target.value)}/>
+        <label for="lastName" style={{ fontSize: '20px'}}>Sisesta oma perenimi:</label>
+        <input type="text" name="lastName" id="lastName" onChange={(e) => setlastName(e.target.value)}/>
+        <label for="schoolName" style={{ fontSize: '20px'}}>Sisesta oma kooli nimi:</label>
+        <input type="text" name="schoolName" id="schoolName" onChange={(e) => setschoolName(e.target.value)}/>
+        <button id="save" onClick={saveLocal}>Kuva andmed</button>
+        <br />
+        {displayData && <div style={{ fontSize: '20px'}}>{displayData}</div>}
+
       </header>
     </div>
   );
